650328bebd4c15a5412d7d6da2f1677b48e1bde7
Colton Wolkins <frostyfrog2@gmail.com>
2013-07-07 17:42:13 -0600

    Fixed alert.py for PEP8 compliance.

--- mk2/plugins/alert.py
+++ mk2/plugins/alert.py
@@ -7,17 +7,19 @@ from mk2.events import StatPlayerCount, Hook
 
 class Alert(Plugin):
     interval = Plugin.Property(default=200)
-    command  = Plugin.Property(default="say {message}")
-    path     = Plugin.Property(default="alerts.txt")
-    
+    command = Plugin.Property(default="say {message}")
+    path = Plugin.Property(default="alerts.txt")
+
     messages = []
     repeating = False
     empty = True
 
     def setup(self):
-        self.register( self.emptyCheck, StatPlayerCount )
+        self.register(self.emptyCheck, StatPlayerCount)
 
-        self.register( self.server_started, Hook, public=True, name="reload-alert", doc='Reload all of the alerts from %s' % self.path )
+        self.register(self.server_started, Hook, public=True,
+                      name="reload-alert",
+                      doc='Reload all of the alerts from %s' % self.path)
 
     def server_started(self, event=None):
         if self.path and os.path.exists(self.path):
@@ -27,15 +29,16 @@ class Alert(Plugin):
                 if l:
                     self.messages.append(l)
             f.close()
-            
+
             if self.messages and not self.repeating:
-                self.console("Repeating interval: %d" % self.interval )
+                self.console("Repeating interval: %d" % self.interval)
                 self.repeating_task(self.repeater, self.interval)
                 self.repeating = True
             if self.messages:
-                self.console( "[Plugin] Alert: Successfully loaded %s" % self.path )
+                self.console("[Plugin] Alert: Successfully loaded %s" %
+                             self.path)
             else:
-                self.console( "[Plugin] Alert: Failed to load %s" % self.path )
+                self.console("[Plugin] Alert: Failed to load %s" % self.path)
 
     def emptyCheck(self, event):
         if event.players_current > 0:
@@ -45,8 +48,10 @@ class Alert(Plugin):
 
     def repeater(self, event):
         if not self.empty and self.messages:
-            self.send_format(self.command, parseColors=True, message=random.choice(self.messages))
+            self.send_format(self.command, parseColors=True,
+                             message=random.choice(self.messages))
         elif not self.empty:
             self.console("[Plugin] Alert: No alerts loaded...")
 
 # vim: set ai et ts=4 sw=4:
+
